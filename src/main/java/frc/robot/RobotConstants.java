8e897c8fb9bd3bd34cedcffa626d447cd29e25ae
Xklost <emscranton@icloud.com>
2022-02-12 14:28:05 -0600

    inital constants file

---
+++ src/main/java/frc/robot/RobotConstants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class RobotConstants {
+
+    public static final int MOTOR_CHANNEL_FLD = 0;
+    public static final int MOTOR_CHANNEL_BLD = 1;
+    public static final int MOTOR_CHANNEL_FRD = 2;
+    public static final int MOTOR_CHANNEL_BRD = 3;
+
+    public static final boolean MOTOR_INVERT_L = false;
+    public static final boolean MOTOR_INVERT_R = true; 
+}
