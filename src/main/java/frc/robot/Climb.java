a21640e6cb59c030e7be57f6fc23cf99a38c4249
Ethan.ScrantonP <emscranton@icloud.com>
2022-02-22 17:56:46 -0600

    Doublesolenoid set up

--- src/main/java/frc/robot/Climb.java
+++ src/main/java/frc/robot/Climb.java
@@ -1,8 +1,17 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 
 public class Climb {
-    
+
+public static Compressor comp = new Compressor(0, PneumaticsModuleType.CTREPCM);
+public static DoubleSolenoid Solenoid_1 = new DoubleSolenoid(null, 0, 1);
+public static DoubleSolenoid Solenoid_2 = new DoubleSolenoid(null, 2, 3);
+public static DoubleSolenoid Solenoid_3 = new DoubleSolenoid(null, 4, 5);
+public static DoubleSolenoid Solenoid_4 = new DoubleSolenoid(null, 6, 7);
 }
