680f536925e1b1d4417d1c4ede8fd9abbc7125ca
Luke Kindelin <kindelin@gmail.com>
2022-02-23 16:49:54 -0600

    Better Climb framework +better pneumatics handling

--- src/main/java/frc/robot/Climb.java
+++ src/main/java/frc/robot/Climb.java
@@ -1,33 +1,53 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PneumaticHub;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Joystick.ButtonType;
-import edu.wpi.first.wpilibj2.command.button.Button;
-import edu.wpi.first.wpilibj.Compressor;
+import pabeles.concurrency.ConcurrencyOps.Reset;
+//import edu.wpi.first.wpilibj.PneumaticHub;
+//import edu.wpi.first.wpilibj.PneumaticsModuleType;
+//import edu.wpi.first.wpilibj.Solenoid;
+//import edu.wpi.first.wpilibj.Joystick.ButtonType;
+//import edu.wpi.first.wpilibj2.command.button.Button;
+//import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 
 public class Climb {
+    private DoubleSolenoid _Solenoid_1;
+    private DoubleSolenoid _Solenoid_2;
+    private DoubleSolenoid _Solenoid_3;
+    private DoubleSolenoid _Solenoid_4;
+    private boolean _climbExtendRetractPrimary;
 
-public static Compressor comp = new Compressor(0, null);
-public static DoubleSolenoid Solenoid_1 = new DoubleSolenoid(null, 0, 1);
-public static DoubleSolenoid Solenoid_2 = new DoubleSolenoid(null, 2, 3);
-public static DoubleSolenoid Solenoid_3 = new DoubleSolenoid(null, 4, 5);
-public static DoubleSolenoid Solenoid_4 = new DoubleSolenoid(null, 6, 7);
-{
+    public Climb() {
 
-if(JOYSTICK_PORT_COPILOT.getrawButton(1) == true)
+        //public static Compressor comp = new Compressor(0, null);
+        this._Solenoid_1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, RobotConstants.Solenoid_1_A, RobotConstants.Solenoid_1_B);
+        
+        //Update the next few lines to match the example from Solenoid #1
+        //public static DoubleSolenoid Solenoid_2 = new DoubleSolenoid(null, 2, 3);
+        //public static DoubleSolenoid Solenoid_3 = new DoubleSolenoid(null, 4, 5);
+        //public static DoubleSolenoid Solenoid_4 = new DoubleSolenoid(null, 6, 7);
+        this.reset();
+    }
+    
+    public void reset() {
+        //reset will move pneumatics system to default retracted state
+        this._Solenoid_1.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void update(boolean _climbExtendRetractPrimary){
+        if(_climbExtendRetractPrimary == true){
+            this._Solenoid_1.set(DoubleSolenoid.Value.kForward);
+        }
+        else{
+            this._Solenoid_1.set(DoubleSolenoid.Value.kReverse);
+        }
+    }
+//if(JOYSTICK_PORT_COPILOT.getrawButton(1) == true)
     { 
-   Solenoid_1.set(DoubleSolenoid.Value.kForward);
-   Solenoid_3.set(DoubleSolenoid.Value.kForward);
-}
-else
-    {
-   Solenoid_1.set(DoubleSolenoid.Value.kReverse);
-   Solenoid_3.set(DoubleSolenoid.Value.kReverse);
+   
+   //Solenoid_3.set(DoubleSolenoid.Value.kForward);
 }
 
-}
+   //Solenoid_3.set(DoubleSolenoid.Value.kReverse);
 }
--- src/main/java/frc/robot/OperatorInterface.java
+++ src/main/java/frc/robot/OperatorInterface.java
@@ -7,8 +7,6 @@ public class OperatorInterface {
     private Joystick copilot_joy;    
 
    
-   
-   
    // Needs clean up
    public OperatorInterface(){
        this.pilot_joy = new Joystick(RobotConstants.JOYSTICK_PORT_PILOT);
@@ -17,9 +15,12 @@ public class OperatorInterface {
     public double leftDriveStick (){
       return this.pilot_joy.getRawAxis(RobotConstants.CONTROLLER_DRIVE_CHANNEL_L) * 0.4;
          }
-      public double rightDriveStick (){
-    return this.pilot_joy.getRawAxis(RobotConstants.CONTROLLER_DRIVE_CHANNEL_R) * 0.4;
+    public double rightDriveStick (){
+      return this.pilot_joy.getRawAxis(RobotConstants.CONTROLLER_DRIVE_CHANNEL_R) * 0.4;
+        }
+    public boolean climbExtendRetractPrimary (){
+      return this.copilot_joy.getRawButton(RobotConstants.CLIMB_EXTEND_RETRACT_PRIMARY);
+        }
       }
-}
       
 // Use Joystick library here: https://first.wpi.edu/FRC/roborio/release2017/docs/java/edu/wpi/first/wpilibj/Joystick.html 
