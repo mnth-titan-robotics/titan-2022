bdab3be13af1f13f0db0f255603f53fe863403df
Ethan.ScrantonP <emscranton@icloud.com>
2022-02-23 15:08:11 -0600

    Climb V1 try 2

--- src/main/java/frc/robot/Climb.java
+++ src/main/java/frc/robot/Climb.java
@@ -16,8 +16,8 @@ public static DoubleSolenoid Solenoid_1 = new DoubleSolenoid(null, 0, 1);
 public static DoubleSolenoid Solenoid_2 = new DoubleSolenoid(null, 2, 3);
 public static DoubleSolenoid Solenoid_3 = new DoubleSolenoid(null, 4, 5);
 public static DoubleSolenoid Solenoid_4 = new DoubleSolenoid(null, 6, 7);
-
 {
+
 if(JOYSTICK_PORT_COPILOT.getrawButton(1) == true)
     { 
    Solenoid_1.set(DoubleSolenoid.Value.kForward);
@@ -28,7 +28,6 @@ else
    Solenoid_1.set(DoubleSolenoid.Value.kReverse);
    Solenoid_3.set(DoubleSolenoid.Value.kReverse);
 }
-
 
 }
 }
--- src/main/java/frc/robot/OperatorInterface.java
+++ src/main/java/frc/robot/OperatorInterface.java
@@ -20,25 +20,6 @@ public class OperatorInterface {
       public double rightDriveStick (){
     return this.pilot_joy.getRawAxis(RobotConstants.CONTROLLER_DRIVE_CHANNEL_R) * 0.4;
       }
-    
+}
       
 // Use Joystick library here: https://first.wpi.edu/FRC/roborio/release2017/docs/java/edu/wpi/first/wpilibj/Joystick.html 
-// Desired call to getrawaxis requires specific controller target. 
-
-// public void teleopPeriodic() {
-// double speed = this.pilot_joy.getrawAxis(RobotConstants.CONTROLLER_DRIVE_CHANNEL_L) * 0.6;
-// double speed2 = CONTROLLER_DRIVE_CHANNEL_R.getrawAxis(5) * 0.6;
-
-// double left = speed
-// double right = speed2
-
-// MOTOR_CHANNEL_BLD.set(left)
-// MOTOR_CHANNEL_FLD.set(left)
-// MOTOR_CHANNEL_BRD.set(right)
-// MOTOR_CHANNEL_FRD.set(right)
-
-
-//    }
-}
-
-
