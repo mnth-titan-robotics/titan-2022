10c849439eb3880d1f851892c778bbd868b85bf7
Ethan.ScrantonP <emscranton@icloud.com>
2022-02-28 16:49:07 -0600

    Finished pnumatic code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -102,8 +102,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     driveSystem.update(Ops.leftDriveStick (), Ops.rightDriveStick());
-    climbsystem.update(Ops.armset1());
-    climbsystem.update(Ops.armset2());
+    climbsystem.update(Ops.armset1(), Ops.armset2());
   }
 
   /** This function is called once when the robot is disabled. */
